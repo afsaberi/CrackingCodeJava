e992246067282e70c7a6de27cfaf7d4e24827e29
Ali <afsaberi23@gmail.com>
2017-02-12 23:35:08 -0500

    Finished 1.1(isUnique)

--- src/isUnique.java
+++ src/isUnique.java
@@ -1,8 +1,23 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
- * Created by Anita on 2017-02-12.
+ * Created by Ali on 2017-02-12.
  */
 public class isUnique {
+    public static boolean hasUniqueChar(String string){
+        HashSet<Character> set= new HashSet<>();
+        for(int i=0;i<string.length();i++){
+            if(set.contains(string.charAt(i))){
+                return false;
+            }
+            else if (string.charAt(i)==' ');
+            else
+                set.add(string.charAt(i));
+        }
+        return true;
+    }
     public static void main(String [] args){
-
+        System.out.println(hasUniqueChar("al of thes ring q"));
     }
 }
